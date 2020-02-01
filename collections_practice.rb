ee28d06958881304093edbf3435193f55b34fb54
fadimata tillman <fadi.tillman@gmail.com>
2020-02-01 22:53:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
 
+ def sort_array_asc(array)
+   array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
+end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+ if a == b
+   0
+ elsif a < b
+   1
+ elsif a > b
+   -1
+ end
+end
+end
+
+def sort_array_char_count(arraynum)
+  arraynum.sort do |a, b|
+ if a.length == b.length
+   0
+ elsif a.length < b.length
+   -1
+ elsif a.length > b.length
+   1
+ end
+end
+end
+
+def swap_elements(array)
+   array [0],array [1],array[2] =  array [0], array [2], array[1]
+  end
+
+  def reverse_array(array)
+    array.reverse
+  end
+
+
+    def kesha_maker(array)
+    new_array =[]
+    array.each do |element|
+      element[2] ="$"
+    new_array << element
+    end
+    new_array
+  end
