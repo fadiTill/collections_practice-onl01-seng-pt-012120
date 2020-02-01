000b72e49364f8198016129cda9e12594e76d477
fadimata tillman <fadi.tillman@gmail.com>
2020-02-01 23:10:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -58,7 +58,7 @@ def swap_elements(array)
   def find_a(array)
     start_with? = []
     array.find do |element|
-    element[0] = "a"
+    element.first ("a")
     start_with? << element
   end
   start_with?
